04eceaa9e605d154dc3edc608bb20bd1c4268a65
bawj <brendanawjang@gmail.com>
2022-07-01 00:40:36 +0800

    Using postcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import * as path from 'path';
+import postcss from 'rollup-plugin-postcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,13 +11,14 @@ export default defineConfig({
     dts({
       insertTypesEntry: true,
     }),
+    postcss(),
   ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'react-polygon-image-cropper',
       formats: ['es', 'umd'],
-      fileName: (format) => `my-lib.${format}.js`,
+      fileName: (format) => `react-polygon-image-cropper.${format}.js`,
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
