144f67d42e8de666ab49fd0b65d874536ef5434e
bawj <brendanawjang@gmail.com>
2022-07-01 01:42:35 +0800

    Remove postcss in favor of manual import stylesheet

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import * as path from 'path';
-import postcss from 'rollup-plugin-postcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +10,6 @@ export default defineConfig({
     dts({
       insertTypesEntry: true,
     }),
-    postcss(),
   ],
   build: {
     lib: {
